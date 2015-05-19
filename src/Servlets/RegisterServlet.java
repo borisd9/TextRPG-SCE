0b4f14b53832309446c934533c9814f5e9a54cf3
yonimv <yonimv@gmail.com>
2015-05-19 16:22:27 +0300

    Finished registration - added validations

--- src/Servlets/RegisterServlet.java
+++ src/Servlets/RegisterServlet.java
@@ -5,6 +5,8 @@ import java.io.PrintWriter;
 import java.security.NoSuchAlgorithmException;
 
 import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,6 +26,7 @@ public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
       
 	RegisterDB db;
+	String contextPath;
 	
     /**
      * @see HttpServlet#HttpServlet()
@@ -47,42 +50,60 @@ public class RegisterServlet extends HttpServlet {
 		final String username = request.getParameter("user");
 		String password = request.getParameter("pwd");
 		final String email = request.getParameter("email");
+		contextPath = request.getContextPath();
 		
-		String hashedUsername = null, hashedPassword = null;
-		Sha1Hex sha1 = new Sha1Hex();
-		try {
-			hashedUsername = sha1.makeSHA1Hash(username);
-			hashedPassword = sha1.makeSHA1Hash(password);
-		} catch (NoSuchAlgorithmException e) {
-			System.out.println("Error creating hash: "+e);
-		}
-		
-		final String auth_code = hashedUsername;
-		final String encPassword = hashedPassword;
-		
-		db.insert(username, encPassword, email);
-		db.insert(username, auth_code);
-			
-		//Send mail
-		Thread t = new Thread(new Runnable() {	
-			@Override
-			public void run() {
-				SendMail sm = new SendMail();
-				sm.registerMail(email, username, auth_code);
-				try {
-					sm.send();
-				} catch (MessagingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+		//check if the fields are blank
+		if(username=="" || password=="" || email=="")
+			response.sendRedirect(contextPath + "/register.jsp?err=1");
+		//check if email is valid
+		else if(!isValidEmail(email))
+			response.sendRedirect(contextPath + "/register.jsp?err=2");
+		else{
+			String hashedUsername = null, hashedPassword = null;
+			Sha1Hex sha1 = new Sha1Hex();
+			try {
+				hashedUsername = sha1.makeSHA1Hash(username);
+				hashedPassword = sha1.makeSHA1Hash(password);
+			} catch (NoSuchAlgorithmException e) {
+				System.out.println("Error creating hash: "+e);
 			}
-		});
-		t.start();
-		
-		RequestDispatcher rd = getServletContext().getRequestDispatcher("/register.jsp");
-        PrintWriter out= response.getWriter();
-        out.println("<font color=blue>Registration complete! Please follow the steps provided in your mail's inbox to activate your account.</font><br/>");
-        rd.include(request, response);
+			
+			final String auth_code = hashedUsername;
+			final String encPassword = hashedPassword;
+			
+			if(!db.insert(username, encPassword, email))
+				response.sendRedirect(contextPath + "/register.jsp?err=3");
+			else{
+				db.insert(username, auth_code);
+				
+				//Send mail
+				Thread t = new Thread(new Runnable() {	
+					@Override
+					public void run() {
+						SendMail sm = new SendMail();
+						sm.registerMail(email, username, auth_code);
+						try {
+							sm.send();
+						} catch (MessagingException e) {
+							System.out.println("Error sending mail: "+e);
+						}
+					}
+				});
+				t.start();
+				
+				response.sendRedirect(contextPath + "/register.jsp?ok=1");
+			}	
+		}
 	}
 
+	public static boolean isValidEmail(String email) {
+	   boolean result = true;
+	   try {
+	      InternetAddress emailAddr = new InternetAddress(email);
+	      emailAddr.validate();
+	   } catch (AddressException ex) {
+	      result = false;
+	   }
+	   return result;
+	}
 }
